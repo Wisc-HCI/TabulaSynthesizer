e3527cdbbec3b4144de063b8338cc1329026c45f
david <david.j.porfirio@gmail.com>
2023-03-11 09:45:18 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
+roslaunch synthesizer ctrl.launch &
 cd bin/stanford-corenlp-4.5.0/
 java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000 &
-echo "launching Tabula synthesizer"
-roslaunch synthesizer ctrl.launch
+
