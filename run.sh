c7332c9d66ab4456d9d12435d70d876d0e7a3125
david <david.j.porfirio@gmail.com>
2023-03-11 00:30:56 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /opt/ros/noetic/setup.bash
+source /catkin_ws/devel/setup.bash
 cd bin/stanford-corenlp-4.5.0/
 java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000 &
-#roslaunch synthesizer ctrl.launch
+roslaunch synthesizer ctrl.launch
